7db4521190ecb84d5b03e69da443c9ce187ee52d
Shangin Vyacheslav <slava-poison@yandex.ru>
2021-02-16 23:58:08 +0400

    1 commit

---
+++ lesson7/DataManager.swift
@@ -0,0 +1,19 @@
+//
+//  DataManager.swift
+//  lesson7
+//
+//  Created by Вячеслав Шангин on 16.02.2021.
+//
+
+class DataManager {
+    let names = ["John", "Carl", "Edward"]
+    let lastNames = ["Norton", "Pitt", "Di Caprio"]
+    let phones = ["88798838833", "33234223", "55364546"]
+    let emails = ["ttt@tutu.ru", "sss@mail.ru", "ksskka@ya.ru"]
+    
+    func getData() -> [Person] {
+        var persons: [Person] = []
+        
+        return persons
+    }
+}
---
+++ lesson7/Person.swift
@@ -0,0 +1,14 @@
+//
+//  Person.swift
+//  lesson7
+//
+//  Created by Вячеслав Шангин on 16.02.2021.
+//
+
+struct Person {
+    let name: String
+    let lastName: String
+    let email: String
+    let tel: String
+}
+
